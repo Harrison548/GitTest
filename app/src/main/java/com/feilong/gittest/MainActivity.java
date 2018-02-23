03066e6b85b4f0ff1169efc3acf3c9f17e839c63
unknown <389770870@qq.com>
2018-02-23 21:22:14 +0800

    创建项目

---
+++ app/src/main/java/com/feilong/gittest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.feilong.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "测试页面";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: 测试页面");
+
+    }
+}
